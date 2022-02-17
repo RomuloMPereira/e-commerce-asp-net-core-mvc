fdcb7806595d1fae1759296ee076ceb15a7d6bda
Romulo Martins Pereira <mp.romulo@gmail.com>
2022-02-17 19:35:47 -0300

    Add Program.cs and Startup.cs

--- LanchesMacMVCNet6/Program.cs
+++ LanchesMacMVCNet6/Program.cs
@@ -1,27 +1,19 @@
-var builder = WebApplication.CreateBuilder(args);
+namespace LanchesMacMVCNet6;
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    public static void Main(string[] args)
+    {
+        //criamos o método de extensão CreateAdminRole
+        CreateHostBuilder(args)
+           .Build()
+           .Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
