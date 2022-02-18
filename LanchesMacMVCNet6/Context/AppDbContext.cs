9438c3d60fbeb30976a1f4af9fa406a8287517d8
Romulo Martins Pereira <mp.romulo@gmail.com>
2022-02-18 20:06:18 -0300

    Configure EF Core

---
+++ LanchesMacMVCNet6/Context/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using LanchesMacMVCNet6.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LanchesMacMVCNet6.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { }
+        public DbSet<Lanche> Lanches { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+    }
+}
