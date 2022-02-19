a13201f38118a95695c5526c6a8ffe35c6ff0f9c
Romulo Martins Pereira <mp.romulo@gmail.com>
2022-02-19 09:37:12 -0300

    Lanche Repository

---
+++ LanchesMacMVCNet6/Repositories/Interfaces/ILancheRepository.cs
@@ -0,0 +1,11 @@
+﻿using LanchesMacMVCNet6.Models;
+
+namespace LanchesMacMVCNet6.Repositories.Interfaces
+{
+    public interface ILancheRepository
+    {
+        IEnumerable<Lanche> Lanches { get; }
+        IEnumerable<Lanche> LanchesPreferidos { get; }
+        Lanche GetLancheById(int lancheId);
+    }
+}
