47ad75fdfd29c310161f94d79aa4640d4f82bdfe
Romulo Martins Pereira <mp.romulo@gmail.com>
2022-02-19 10:10:50 -0300

    Lanche Controller with List Action/ Scaffolding

---
+++ LanchesMacMVCNet6/Controllers/LancheController.cs
@@ -0,0 +1,21 @@
+﻿using LanchesMacMVCNet6.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMacMVCNet6.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepository _lancheRepository;
+
+        public LancheController(ILancheRepository lancheRepository)
+        {
+            _lancheRepository = lancheRepository;
+        }
+        public IActionResult List()
+        {
+            var lanches = _lancheRepository.Lanches;
+
+            return View(lanches);
+        }
+    }
+}
