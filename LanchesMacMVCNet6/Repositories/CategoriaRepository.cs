4e7c9b4410b39ee0ee1d084d8f8372868957ce0d
Romulo Martins Pereira <mp.romulo@gmail.com>
2022-02-19 09:28:07 -0300

    Categoria Repository

---
+++ LanchesMacMVCNet6/Repositories/CategoriaRepository.cs
@@ -0,0 +1,17 @@
+﻿using LanchesMacMVCNet6.Context;
+using LanchesMacMVCNet6.Models;
+using LanchesMacMVCNet6.Repositories.Interfaces;
+
+namespace LanchesMacMVCNet6.Repositories
+{
+    public class CategoriaRepository : ICategoriaRepository
+    {
+        private readonly AppDbContext _context;
+
+        public CategoriaRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Categoria> Categorias => _context.Categorias;
+    }
+}
---
+++ LanchesMacMVCNet6/Repositories/Interfaces/ICategoriaRepository.cs
@@ -0,0 +1,9 @@
+﻿using LanchesMacMVCNet6.Models;
+
+namespace LanchesMacMVCNet6.Repositories.Interfaces
+{
+    public interface ICategoriaRepository
+    {
+        IEnumerable<Categoria> Categorias { get; }
+    }
+}
