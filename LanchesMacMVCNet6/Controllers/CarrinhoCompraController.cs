3429886cad401ea304ea90193c7ad870878f8352
Romulo Martins Pereira <mp.romulo@gmail.com>
2022-02-22 17:20:10 -0300

    CarrinhoCompra Controller

--- LanchesMacMVCNet6/Controllers/CarrinhoCompraController.cs
+++ LanchesMacMVCNet6/Controllers/CarrinhoCompraController.cs
@@ -28,4 +28,26 @@ namespace LanchesMacMVCNet6.Controllers
 
             return View(carrinhoCompraVM);
         }
+
+        public RedirectToActionResult AdicionarItemNoCarrinhoCompra(int lancheId)
+        {
+            var lancheSelecionado = _lancheRepository.Lanches.FirstOrDefault(p => p.LancheId == lancheId);
+
+            if (lancheSelecionado != null)
+            {
+                _carrinhoCompra.AdicionarAoCarrinho(lancheSelecionado);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult RemoverItemDoCarrinhoCompra(int lancheId)
+        {
+            var lancheSelecionado = _lancheRepository.Lanches.FirstOrDefault(p => p.LancheId == lancheId);
+            if (lancheSelecionado != null)
+            {
+                _carrinhoCompra.RemoverDoCarrinho(lancheSelecionado);
+            }
+            return RedirectToAction("Index");
+        }
+    }
 }
