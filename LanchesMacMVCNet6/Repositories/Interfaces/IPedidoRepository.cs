d6ac6db290bd62786b310cc56b26a8b9c95629d8
Romulo Martins Pereira <mp.romulo@gmail.com>
2022-02-23 16:25:59 -0300

    Pedido Repository (create, get, and get by Id)

---
+++ LanchesMacMVCNet6/Repositories/Interfaces/IPedidoRepository.cs
@@ -0,0 +1,11 @@
+﻿using LanchesMacMVCNet6.Models;
+
+namespace LanchesMacMVCNet6.Repositories.Interfaces
+{
+    public interface IPedidoRepository
+    {
+        void CriarPedido(Pedido pedido);
+        Pedido GetPedidoById(int pedidoId);
+        List<Pedido> GetPedidos();
+    }
+}
