b8e953725e9f080c707fc903fed9dcfe1be67df5
Romulo Martins Pereira <mp.romulo@gmail.com>
2022-02-23 17:25:05 -0300

    Pedido Controller/ Checkout Action (GET and POST)

---
+++ LanchesMacMVCNet6/Controllers/PedidoController.cs
@@ -0,0 +1,61 @@
+﻿using LanchesMacMVCNet6.Models;
+using LanchesMacMVCNet6.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMacMVCNet6.Controllers
+{
+    public class PedidoController : Controller
+    {
+        private readonly IPedidoRepository _pedidoRepository;
+        private readonly CarrinhoCompra _carrinhoCompra;
+
+        public PedidoController(IPedidoRepository pedidoRepository, CarrinhoCompra carrinhoCompra)
+        {
+            _pedidoRepository = pedidoRepository;
+            _carrinhoCompra = carrinhoCompra;
+        }
+
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(Pedido pedido)
+        {
+            decimal precoTotalPedido = 0.0m;
+            int totalItensPedido = 0;
+
+            List<CarrinhoCompraItem> items = _carrinhoCompra.GetCarrinhoCompraItens();
+
+            _carrinhoCompra.CarrinhoCompraItens = items;
+
+            if (_carrinhoCompra.CarrinhoCompraItens.Count == 0)
+            {
+                ModelState.AddModelError("", "Seu carrinho esta vazio, que tal incluir um lanche...");
+            }
+
+            foreach (var item in items)
+            {
+                totalItensPedido += item.Quantidade;
+                precoTotalPedido += (item.Lanche.Preco * item.Quantidade);
+            }
+
+            pedido.TotalItensPedido = totalItensPedido;
+
+            pedido.PedidoTotal = precoTotalPedido;
+
+            if (ModelState.IsValid)
+            {
+                _pedidoRepository.CriarPedido(pedido);
+
+                ViewBag.CheckoutCompletoMensagem = "Obrigado pelo seu pedido :) ";
+                ViewBag.TotalPedido = _carrinhoCompra.GetCarrinhoCompraTotal();
+
+                _carrinhoCompra.LimparCarrinho();
+                return View("~/Views/Pedido/CheckoutCompleto.cshtml", pedido);
+            }
+            return View(pedido);
+        }
+    }
+}
