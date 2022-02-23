20f4e5c3451ece447da504a50eecf92396a51b4a
Romulo Martins Pereira <mp.romulo@gmail.com>
2022-02-23 15:18:29 -0300

    Expand the Domain Model (Pedido and PedidoDetalhe)/ Register in the DbContext

--- LanchesMacMVCNet6/Context/AppDbContext.cs
+++ LanchesMacMVCNet6/Context/AppDbContext.cs
@@ -10,5 +10,7 @@ namespace LanchesMacMVCNet6.Context
         public DbSet<Lanche> Lanches { get; set; }
         public DbSet<Categoria> Categorias { get; set; }
         public DbSet<CarrinhoCompraItem> CarrinhoCompraItens { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<PedidoDetalhe> PedidoDetalhes { get; set; }
     }
 }
