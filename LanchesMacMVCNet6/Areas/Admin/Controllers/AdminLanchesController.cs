9dddb1934f2b8aaedae7acc54e2a34acdea85551
Romulo Martins Pereira <mp.romulo@gmail.com>
2022-02-25 16:43:45 -0300

    Pagination and Filter (Lanches Admin)

--- LanchesMacMVCNet6/Areas/Admin/Controllers/AdminLanchesController.cs
+++ LanchesMacMVCNet6/Areas/Admin/Controllers/AdminLanchesController.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using LanchesMacMVCNet6.Context;
 using LanchesMacMVCNet6.Models;
 using Microsoft.AspNetCore.Authorization;
+using ReflectionIT.Mvc.Paging;
 
 namespace LanchesMacMVCNet6.Areas.Admin.Controllers
 {
@@ -23,10 +24,27 @@ namespace LanchesMacMVCNet6.Areas.Admin.Controllers
         }
 
         // GET: Admin/AdminLanches
-        public async Task<IActionResult> Index()
+        //public async Task<IActionResult> Index()
+        //{
+        //    var appDbContext = _context.Lanches.Include(l => l.Categoria);
+        //    return View(await appDbContext.ToListAsync());
+        //}
+        public async Task<IActionResult> Index(string filter, int pageindex = 1, string sort = "Nome")
         {
-            var appDbContext = _context.Lanches.Include(l => l.Categoria);
-            return View(await appDbContext.ToListAsync());
+            var resultado = _context.Lanches.Include(l => l.Categoria)
+                           .AsQueryable();
+
+
+            if (!string.IsNullOrWhiteSpace(filter))
+            {
+                resultado = resultado.Where(p => p.Nome.Contains(filter));
+            }
+
+            var model = await PagingList.CreateAsync(resultado, 5, pageindex, sort, "Nome");
+
+            model.RouteValue = new RouteValueDictionary { { "filter", filter } };
+
+            return View(model);
         }
 
         // GET: Admin/AdminLanches/Details/5
